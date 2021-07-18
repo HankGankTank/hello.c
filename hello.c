d65d2b84cdcc46d2abe3ae1134b117b3d99bb4de
HankGankTank <87589748+HankGankTank@users.noreply.github.com>
2021-07-18 10:32:09 +0800

    Update hello.c
    Edit name

--- hello.c
+++ hello.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>   
 int main()
 {
-	printf("Hello World\n");   // waw hello Richard and ok
+	printf("Hello World\n");   // Hello Dr.Richard and Linghan
  	exit(0);         // end of the program
 }
